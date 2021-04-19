07cc8c93db484e01f6c526bedec81ac9ded13920
jvberdec <44758976+jvberdec@users.noreply.github.com>
2021-04-19 18:36:21 -0400

    added header files

---
+++ Diop_path.h
@@ -0,0 +1,15 @@
+//
+//  Diop_path.h
+//  OS Project Shell
+//
+//  Created by Julia Berdecia on 4/19/21.
+//
+
+#ifndef Diop_path_h
+#define Diop_path_h
+
+void readFile(int, int);
+int path(void);
+
+
+#endif /* Diop_path_h */
---
+++ Wu_exit.h
@@ -0,0 +1,15 @@
+////
+////  Wu_exit.h
+////  OS Project Shell
+////
+////  Created by Julia Berdecia on 4/19/21.
+////
+//
+//#ifndef Wu_exit_h
+//#define Wu_exit_h
+//
+//void exit(char commands[1024][1024],int count);
+//
+//
+//
+//#endif /* Wu_exit_h */
