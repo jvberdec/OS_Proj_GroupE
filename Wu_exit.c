c2c05abf156c0bfbfc88b472073208bbcdb01e03
huiruwu <31498066+huiruwu@users.noreply.github.com>
2021-04-23 16:45:14 -0400

    Update Wu_exit.c

--- Wu_exit.c
+++ Wu_exit.c
@@ -8,32 +8,40 @@ void my_exit(char commands[1024][50],int count)
 {	//collects the last 4 commands
 	printf("Printing the last 4 commands used \n");
 	int start = count;
+	// if start is less then 4, initializes it to 0
 	if(start < 4)
 	{
 		start = 0;
 	}
 	else
 	{
+		//gets the last 4 commands
 		start -= 4;
 	}
 
 	for (int i = start; i < count; i++)
 	{
+		// prints the last commands in a list
 		printf("%d : %s\n", i + 1, commands[i]);
 	}
 	//creates a fork
 	int pid = fork();
 
 	if(pid < 0)
-	{
+	{	
+		//error checking
 		printf("Fork failed");
 	}
 	//child
 	if(pid == 0)
 	{
+		//defines args
 		char *args[3];
+		//will execute ls
 		args[0] = "ls";
+		//will execute -l
 		args[1] = "-l";
+		//end of the array
 		args[2] = NULL;
 		//executes ls and -l
 		execvp(args[0],args);
@@ -43,15 +51,21 @@ void my_exit(char commands[1024][50],int count)
 	//parent
 	else if (pid >0)
 	{
+		//waits for the child to finish
 		wait(NULL);
 		printf("\n -> Are you sure you want to exit? If yes, press ENTER.");
+		// requires you to press enter/return to continue
 		getchar();
-		 FILE *f = fopen("file.txt","w");
+		//creates a file called history.txt
+		FILE *f = fopen("history.txt","w");
 		 	for (int i =0; i < count; i++)
 		 	{
+				//prints the last 4 commands into a file called history.txt
 		 		fprintf(f,"%s\n",commands[i]);
 		 	}
+			// closes file
 		 	fclose(f);
+	//closing statements	
         printf("SHELL EXITED \n");
         printf("By Group E: \n");
         printf("Group Leader/Shell: Julia Berdecia \n");
